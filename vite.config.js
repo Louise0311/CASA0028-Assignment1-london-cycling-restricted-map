4914deb51d98e51117ff2fb9f08298b9e598a301
Louise0311 <Y.shuting@outlook.com>
2026-02-22 14:41:46 +0000

    Fix asset path by adding base to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/CASA0028-Assignment1-london-cycling-restricted-map/',
 })
